c83af5c9d6e7f949ca0e5303484e458aa9ccb38b
ruben.presa <rubenebur7@gmail.com>
2016-11-24 12:37:21 +0100

    Implementació en la clase Dades, a falta de completar "etiqueta_llista_enters"

--- Dades.cc
+++ Dades.cc
@@ -1,1 +1,21 @@
 #include "Dades.hh"
+
+void Dades::afegir_dada_entera(string index, int dada) {
+    valor_dades_definides.emplace(index,dada);
+}
+
+void Dades::afegir_dada_llista(string index, list<int>& dades){
+    valor_dades_definides.emplace(index,dades);
+}
+
+bool Dades::etiqueta_list_enters(string etiqueta) {
+
+}
+
+int Dades::valor_enter(string etiqueta) {
+    return valor_dades_definides[etiqueta];
+}
+
+list Dades::valor_llista(string etiqueta) {
+    return valor_llistes_definides[etiqueta];
+}
