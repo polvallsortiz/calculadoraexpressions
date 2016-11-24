19b721335a29548a5a4dd1d3e9527b12670a2851
polvallsortiz <pol.valls.ortiz@est.fib.upc.edu>
2016-11-24 19:57:41 +0100

    Llegir Expressió Completat Totalment (Provat en banc de proves)

--- Expressio.cc
+++ Expressio.cc
@@ -5,33 +5,20 @@
 #include "Operacions.hh"
 
 void Expressio::llegir_expressio(string s) {
+    s.erase(0, 1);
+    s.erase(s.size()-1);
     istringstream iss(s);
-    string act;
-    iss >> act;
-    act.erase(0, 1);
-    int parcount = 0;
-    llista_expressio.push_back(act);
-    string resultat;
-    while (iss >> act) {
-        resultat = act;
-        if (act[0] == '(') {
-            ++parcount;
-            resultat = act;
+    string frag = "";
+    string tmp = "";
+    int c = 0;
+    while (iss >> tmp){
+        for(int i = 0; tmp[i] == '('; i++)c++;
+        for(int i = tmp.size() - 1; tmp[i] == ')'; i--)c--;
+        frag += (frag == "" ? "" : " ") + tmp;
+        if(c == 0){
+            llista_expressio.push_back(frag);
+            frag = "";
         }
-        while (parcount != 0) {
-            iss >> act;
-            if (act[0] == '(') ++parcount;
-            resultat += " " + act;
-            while (act[act.size() - 1] == ')') {
-                --parcount;
-                act.erase(act.size() - 1);
-            }
-        }
-        llista_expressio.push_back(resultat);
+
     }
-    list<string>::iterator it = llista_expressio.end();
-    --it;
-    string ambpar = *it;
-    ambpar.erase(ambpar.size() - 1);
-    *it = ambpar;
 }
