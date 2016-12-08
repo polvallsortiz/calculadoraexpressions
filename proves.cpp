382070173d8ab0bc0132b686b27e25c7c4b47c30
polvallsortiz <pol.valls.ortiz@est.fib.upc.edu>
2016-12-08 01:25:50 +0100

    At proves.cpp all the recursive implementation and all the predefined functions working perfectly (EXCEPT the questions at issue #5). @MrNbr we need to try it with some really hard expressions to prove that it's ok.

--- proves.cpp
+++ proves.cpp
@@ -11,7 +11,7 @@
 using namespace std;
 
 struct Resultat {
-    vector<int> llista;
+    list<int> llista;
     int resultat;
     string desc;
 };
@@ -42,6 +42,7 @@ Resultat evaluar(list<string> llista_expressio) {
     if(op == "+") {
         ++it;
         string l = *it;
+        cerr << l << endl;
         list<string> llistal;
         llegir_expressio(l,llistal);
         Resultat lres = evaluar(llistal);
@@ -59,7 +60,7 @@ Resultat evaluar(list<string> llista_expressio) {
         ++it;
         string l = *it;
         list <string> llistal;
-        llegir_expressio(l.llistal);
+        llegir_expressio(l,llistal);
         Resultat lres = evaluar(llistal);
         lres.resultat = -lres.resultat;
         lres.desc = "enter";
@@ -100,7 +101,7 @@ Resultat evaluar(list<string> llista_expressio) {
         list<string> llistal;
         llegir_expressio(l,llistal);
         Resultat lres = evaluar(llistal);
-        lres.llista = lres.llista.pop_front();
+        lres.llista.pop_front();
         lres.desc = "llista";
         return lres;
     }
@@ -181,30 +182,144 @@ Resultat evaluar(list<string> llista_expressio) {
 
         }
         else {
-            if(lres.desc == "bool" and rres.desc == "bool") {
-                if(lres.resultat == rres.resultat) {
-                    tres.resultat = 1;
-                    tres.desc = "bool";
-                }
-                else {
-                    tres.resultat = 0;
-                    tres.desc = "bool";
-                }
-            }
-            else {
-                    tres.desc = "indefinit";
-            }
+            tres.desc = "indefinit";
         }
         return tres;
     }
-    //TOTES LES OP'S
-    else {
-            Resultat tres;
-            istringstream stoi(*it);
-            int res;
-            stoi >> res;
-            tres.resultat = res;
+    if(op == "not") {
+        ++it;
+        string l = *it;
+        list<string> llistal;
+        llegir_expressio(l,llistal);
+        Resultat lres = evaluar(llistal);
+        Resultat tres;
+        if(lres.desc == "bool") {
+            if(lres.resultat == 0) tres.resultat = 1;
+            else tres.resultat = 0;
+            tres.desc = "bool";
+        }
+        else tres.desc = "indefinit";
         return tres;
+    }
+    if(op == "and") {
+        ++it;
+        string l = *it;
+        list<string> llistal;
+        llegir_expressio(l,llistal);
+        Resultat lres = evaluar(llistal);
+        ++it;
+        string r = *it;
+        list<string> llistar;
+        llegir_expressio(r,llistar);
+        Resultat rres = evaluar(llistar);
+        Resultat tres;
+        if(lres.desc == "bool" and rres.desc == "bool") {
+            if(lres.resultat == 1 and rres.resultat == 1) {
+                tres.resultat = 1;
+                tres.desc = "bool";
+            }
+            else {
+                tres.resultat = 0;
+                tres.desc = "bool";
+            }
+        }
+        else tres.desc = "indefinit";
+        return tres;
+    }
+    if(op == "or") {
+        ++it;
+        string l = *it;
+        list<string> llistal;
+        llegir_expressio(l,llistal);
+        Resultat lres = evaluar(llistal);
+        ++it;
+        string r = *it;
+        list<string> llistar;
+        llegir_expressio(r,llistar);
+        Resultat rres = evaluar(llistar);
+        Resultat tres;
+        if(lres.desc == "bool" and rres.desc == "bool") {
+            if(lres.resultat == 1 or rres.resultat == 1) {
+                tres.resultat = 1;
+                tres.desc = "bool";
+            }
+            else {
+                tres.resultat = 0;
+                tres.desc = "bool";
+            }
+        }
+        else tres.desc = "indefinit";
+        return tres;
+    }
+    if(op == "if") {
+        ++it;
+        string x = *it;
+        list<string> llistax;
+        llegir_expressio(x,llistax);
+        Resultat xres = evaluar(llistax);
+        ++it;
+        string y = *it;
+        list<string> llistay;
+        llegir_expressio(y,llistay);
+        Resultat yres = evaluar(llistay);
+        ++it;
+        string z = *it;
+        list<string> llistaz;
+        llegir_expressio(z,llistaz);
+        Resultat zres = evaluar(llistaz);
+        Resultat tres;
+        if(xres.desc == "bool") {
+            if(xres.resultat == 0) {
+                if(yres.desc == "llista") {
+                    tres.desc = "llista";
+                    tres.llista = yres.llista;
+                }
+                if(yres.desc == "enter") {
+                    tres.desc = "enter";
+                    tres.resultat = yres.resultat;
+                }
+                if(yres.desc == "bool") {
+                    tres.desc = "bool";
+                    tres.resultat = yres.resultat;
+                }
+            }
+            else {
+                if(zres.desc == "llista") {
+                    tres.desc = "llista";
+                    tres.llista = zres.llista;
+                }
+                if(zres.desc == "enter") {
+                    tres.desc = "enter";
+                    tres.resultat = zres.resultat;
+                }
+                if(zres.desc == "bool") {
+                    tres.desc = "bool";
+                    tres.resultat = zres.resultat;
+                }
+            }
+        }
+        else tres.desc = "indefinit";
+        return tres;
+    }
+    else {
+        if(llista_expressio.size() == 1) { //ENTER SOL
+            Resultat tres;
+            istringstream enter(*it);
+            enter >> tres.resultat;
+            tres.desc = "enter";
+            return tres;
+        }
+        else { //LLISTA
+            Resultat tres;
+            while(it != llista_expressio.end()) {
+                istringstream enter(*it);
+                int temp;
+                enter >> temp;
+                tres.llista.push_back(temp);
+                ++it;
+            }
+            return tres;
+        }
     }
 }
 
@@ -213,9 +328,27 @@ int main() {
     string frase;
     getline(cin,frase);
     llegir_expressio(frase,llista);
-    Resultat res;
-    res = evaluar(llista);
-    cout << res.resultat << endl;
-
+    list<string>::iterator it = llista.begin();
+    Resultat res = evaluar(llista);
+    if(res.desc == "bool") {
+        if(res.resultat == 1) {
+            cout << res.resultat << " " << "TRUE" << endl;
+        }
+        else {
+            cout << res.resultat << " " << "FALSE" << endl;
+        }
+    }
+    if(res.desc == "enter") cout << res.resultat << " " << "ENTER" << endl;
+    if(res.desc == "llista") {
+        list<int>::iterator it = res.llista.begin();
+        while(it != res.llista.end()) {
+            cout << *it << " ";
+            ++it;
+        }
+        cout << endl << "LLISTA" << endl;
+    }
+    if(res.desc == "indefinit") {
+        cout << "******** INDEFINIT ********" << endl;
+    }
 }
 
